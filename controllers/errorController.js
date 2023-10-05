bdd6b0fa85ae04ed5bb8b62c49afdb49df5be131
JasontheOmnivorous <marsjason485@gmail.com>
2023-10-05 13:26:58 +0630

    pw encryption and user modeling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,48 +1,72 @@
 const AppError = require('./../utils/appError');
 
 const handleCastErrorDB = err => {
-    // handle searching non-existing tour ids
-    const message = `Invalid ${err.path}: ${err.value}.`;
-    // we basically mark it to be an AppError, so that it can set it to isOperational = true
-    return new AppError(message, 400);
-}
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
 
-const sendErrDev = (err, res) => {
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
     res.status(err.statusCode).json({
-        status: err.status,
-        error: err,
-        message: err.message,
-        stack: err.stack
+      status: err.status,
+      message: err.message
     });
-}
 
-const sendErrProd = (err, res) => {
-    // we wanna show as less-detailed error as possible to the client in production
-    if (err.isOperational === true) { // we only wanna send operational errors to the client because they dont need to know programming errors
-        res.status(err.statusCode).json({
-            status: err.status,
-            message: err.message
-        })
-    } else {
-        console.error(err);
-        res.status(500).json({
-            status: 'error',
-            message: 'Oops! Something went wrong.'
-        })
-    }
-}
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error('ERROR 💥', err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong!'
+    });
+  }
+};
 
 module.exports = (err, req, res, next) => {
-    // console.log(err.stack); 
-    err.statusCode = err.statusCode || 500;
-    err.status = err.status || 'error';
+  // console.log(err.stack);
 
-    if (process.env.NODE_ENV === 'development') {
-        sendErrDev(err, res);
-    } else if (process.env.NODE_ENV === 'production') {
-        let error = {...err};
-        if (error.name === 'CastError') error = handleCastErrorDB(error);
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
 
-        sendErrProd(error, res);
-    }
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+  else if (process.env.NODE_ENV === 'production') {
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
+  }
 };
---
+++ models/userModel.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'User name required.'],
+        minlength: [5, 'User name must be longer than or equal to 10 characters.'],
+        maxlength: [20, 'User name must be shorter than or equal to 20 characters.']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email required.'],
+        unique: [true, 'Email must be unique.'],
+        lowercase: true,
+        validate: [validator.isEmail, 'Please provide a valida email.']
+    },
+    photo: String,
+    password: {
+        type: String,
+        required: [true, 'Password required.'],
+        minlength: [8, 'Passoword should have at least 8 characters.']
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'Please reconfirm password.'],
+        validate: {
+            // this validation only works on create and save methods
+            validator: function(val) {
+                return val === this.password; // validate if the confirmed password is same with password
+            },
+            message: 'Passwords are not the same.'
+        }
+    }
+});
+
+// password encryption using bcrypt algorithm and mongoDB pre save hook
+userSchema.pre('save', async function (next) {
+    // if the password is not modified, we dont wanna encrypt it again
+    if (!this.isModified('password')) return next();
+
+    // hash (encrypt) the password before saving into DB
+    // second arg is the measurement of CPU intensity to use in this operation
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined; // delete the confirmed password after hashing the actual pasword
+    next();
+})
+
+module.exports = mongoose.model('User', userSchema);
