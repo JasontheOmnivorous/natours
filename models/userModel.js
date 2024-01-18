b9c7dd46e8548305301e6df59e544fe1d55c6643
JasontheOmnivorous <marsjason485@gmail.com>
2024-01-18 09:08:48 +0630

    roles and permissions

--- models/userModel.js
+++ models/userModel.js
@@ -3,68 +3,82 @@ const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, 'User name required.'],
-        minlength: [5, 'User name must be longer than or equal to 10 characters.'],
-        maxlength: [20, 'User name must be shorter than or equal to 20 characters.']
+  name: {
+    type: String,
+    required: [true, 'User name required.'],
+    minlength: [5, 'User name must be longer than or equal to 10 characters.'],
+    maxlength: [
+      20,
+      'User name must be shorter than or equal to 20 characters.',
+    ],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email required.'],
+    unique: [true, 'Email must be unique.'],
+    lowercase: true, // convert the input email to all lowercase
+    validate: [validator.isEmail, 'Please provide a valida email.'],
+  },
+  photo: String,
+  role: {
+    type: String,
+    enum: ['admin', 'user', 'guide', 'lead-guide'],
+    default: 'user',
+  },
+  password: {
+    type: String,
+    required: [true, 'Password required.'],
+    minlength: [10, 'Passoword should have at least 8 characters.'],
+    select: false, // should hide the password eventhough we encrypted it
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please reconfirm password.'],
+    validate: {
+      // this validation only works on create and save methods
+      validator: function (val) {
+        return val === this.password; // validate if the confirmed password is same with password
+      },
+      message: 'Passwords are not the same.',
     },
-    email: {
-        type: String,
-        required: [true, 'Email required.'],
-        unique: [true, 'Email must be unique.'],
-        lowercase: true, // convert the input email to all lowercase
-        validate: [validator.isEmail, 'Please provide a valida email.']
-    },
-    photo: String,
-    password: {
-        type: String,
-        required: [true, 'Password required.'],
-        minlength: [10, 'Passoword should have at least 8 characters.'],
-        select: false // should hide the password eventhough we encrypted it
-    },
-    passwordConfirm: {
-        type: String,
-        required: [true, 'Please reconfirm password.'],
-        validate: {
-            // this validation only works on create and save methods
-            validator: function(val) {
-                return val === this.password; // validate if the confirmed password is same with password
-            },
-            message: 'Passwords are not the same.'
-        }
-    },
-    passwordChangedAt: Date // if this property exists, then the user has changed the password
+  },
+  passwordChangedAt: Date, // if this property exists, then the user has changed the password
 });
 
 // password encryption using bcrypt algorithm and mongoDB pre save hook
 userSchema.pre('save', async function (next) {
-    // if the password is not modified, we dont wanna encrypt it again
-    if (!this.isModified('password')) return next();
+  // if the password is not modified, we dont wanna encrypt it again
+  if (!this.isModified('password')) return next();
 
-    // hash (encrypt) the password before saving into DB
-    // second arg is the measurement of CPU intensity to use in this operation
-    this.password = await bcrypt.hash(this.password, 12);
-    this.passwordConfirm = undefined; // delete the confirmed password after hashing the actual pasword
-    next();
+  // hash (encrypt) the password before saving into DB
+  // second arg is the measurement of CPU intensity to use in this operation
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined; // delete the confirmed password after hashing the actual pasword
+  next();
 });
 
 // Instance method
-userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
-    // we can't use this to point the current document because we've already hid it with select: false
-    // bcrypt will calculate and compare these two if they are the same, regardless of they're hash or not
-    return await bcrypt.compare(candidatePassword, userPassword);
-}
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword,
+) {
+  // we can't use this to point the current document because we've already hid it with select: false
+  // bcrypt will calculate and compare these two if they are the same, regardless of they're hash or not
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 userSchema.methods.passwordChangedAfter = function (JWTTimestamp) {
-    if (this.passwordChangedAt) {
-        // parse passwordChangedAt into seconds form
-        // divide with 1000 because getTime give us milliseconds
-        const changedTimeStamp = parseInt(this.passwordChangedAt.getTime() / 1000, 10); // avoid floats using parseInt and base 10 arg
-        return JWTTimestamp < changedTimeStamp; // JWT issued at time must be less than password changed time 
-    }
+  if (this.passwordChangedAt) {
+    // parse passwordChangedAt into seconds form
+    // divide with 1000 because getTime give us milliseconds
+    const changedTimeStamp = parseInt(
+      this.passwordChangedAt.getTime() / 1000,
+      10,
+    ); // avoid floats using parseInt and base 10 arg
+    return JWTTimestamp < changedTimeStamp; // JWT issued at time must be less than password changed time
+  }
 
-    return false; // false by default (never changed password)
-}
+  return false; // false by default (never changed password)
+};
 
 module.exports = mongoose.model('User', userSchema);
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,39 +1,43 @@
 const express = require('express');
 // exports.TourController.controllers => object destructuring
-const {getAllTours, createTour, getTour, updateTour, deleteTour, aliasTopTours, getTourStats, getMonthlyPlan, getSlug} = require('./../controllers/tourController');
-const {protect} = require('./../controllers/authController');
+const {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
+  getSlug,
+} = require('./../controllers/tourController');
+const { protect, restrictTo } = require('./../controllers/authController');
 const router = express.Router(); // create mount router for tours
 
 // // param middleware => a middleware that only runs for certain parameter in the URL
-// router.param('id', checkId); 
+// router.param('id', checkId);
 
 /* Route Chaining */
 // this way, we only have to change the code once let's say when we need to change the route
-router 
-.route('/top-5-cheap')
-.get(aliasTopTours, getAllTours); // route for aliasing
+router.route('/top-5-cheap').get(aliasTopTours, getAllTours); // route for aliasing
 
 // router
 // .route('/:slug')
 // .get(getSlug)
 
-router
-.route('/stats')
-.get(getTourStats); // route for aggregation
+router.route('/stats').get(getTourStats); // route for aggregation
+
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router
-.route('/monthly-plan/:year')
-.get(getMonthlyPlan);
+  .route('/') // we only need to specify needed routes because the mounting route already got the root path
+  .get(protect, getAllTours) // protect seeing all tours from unauthorized user
+  .post(createTour); // can also chain HTTP verbs
 
 router
-   .route('/') // we only need to specify needed routes because the mounting route already got the root path
-   .get(protect, getAllTours) // protect seeing all tours from unauthorized user
-   .post(createTour); // can also chain HTTP verbs
-
-router
-   .route('/:id')
-   .get(getTour) 
-   .patch(updateTour)
-   .delete(deleteTour);
+  .route('/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
 
 module.exports = router; // export our tour route
