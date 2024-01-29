2e4c2e9119db295df0154f6754642d98064493e2
JasontheOmnivorous <marsjason485@gmail.com>
2024-01-29 20:28:20 +0630

    geospatial data for locations per tour

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -3,52 +3,55 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModel');
 
-dotenv.config({ path: './config.env' }); 
+dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD); 
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
 
-mongoose.connect(DB, {
-    useNewUrlParser: true, 
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
-})
-.then(connection => {
+    useFindAndModify: false,
+  })
+  .then((connection) => {
     console.log('DB connection successful');
-});
+  });
 
 // READ JSON FILE
 // this is read as a string so we need to parse this guy up into JS array of objects
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')); 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
 
 // IMPORT DATA INTO DATABASE
 const importData = async () => {
-    try {
-        await Tour.create(tours); // simply create and save the JSON into DB using mongoose
-        console.log('Data successfully loaded!');
-    } catch (err) {
-        console.log(err);
-    }
-    process.exit(); // exit the node environment after executing this function
-}
+  try {
+    await Tour.create(tours); // simply create and save the JSON into DB using mongoose
+    console.log('Data successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit(); // exit the node environment after executing this function
+};
 
 // DELETE ALL DATA FROM DATABASE
 const deleteData = async () => {
-    try {
-        // deleteMany method will delete everything in the database if we didnt pass any argument to it
-        await Tour.deleteMany(); 
-        console.log('Data successfully deleted!');
-    } catch (err) {
-        console.log(err);
-    }
-    process.exit();
-}
-
+  try {
+    // deleteMany method will delete everything in the database if we didnt pass any argument to it
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
 
 // catch flags from terminal and control the import/delete flow
 if (process.argv[2] === '--import') {
-    importData();
+  importData();
 } else if (process.argv[2] === '--delete') {
-    deleteData();
+  deleteData();
 }
 
 // // this guy log out the command line arguments in a form of array of strings
--- models/tourModel.js
+++ models/tourModel.js
@@ -9,110 +9,152 @@ const { isAlpha } = require('validator');
 
 // schema is like a set of rules defined to create our model, which will act as blue print to build our documents
 // schema (set of rules) => model (blue print to build a document) => document (actual data structure)
-const tourSchema = new mongoose.Schema({
+const tourSchema = new mongoose.Schema(
+  {
     // This is called schema type options. Now the name data is essential for a document, we can't omit it
     name: {
-        type: String,
-        // required stuff is called validator, because it is used to validate our data 
-        required: [true, "A tour must have a name."], // second element is error message to show up if we omit it
-        unique: true, // the name has to be a unique value,
-        trim: true, // trims the whitespaces in the beginning and ending of summery input
-        // data validation
-        maxlength: [40, 'Tour name must be shorter than or equal to 40 characters.'],
-        minlength: [10, 'Tour name must be longer than or equal to 10 characters.'],
-        // this dude doesnt even allow spaces for the name so i'll leave it here just for demo of validator library
-        // validate: [isAlpha, 'Tour name must only contains alphabets.'] // method that validates if the input data are alphabets or not
+      type: String,
+      // required stuff is called validator, because it is used to validate our data
+      required: [true, 'A tour must have a name.'], // second element is error message to show up if we omit it
+      unique: true, // the name has to be a unique value,
+      trim: true, // trims the whitespaces in the beginning and ending of summery input
+      // data validation
+      maxlength: [
+        40,
+        'Tour name must be shorter than or equal to 40 characters.',
+      ],
+      minlength: [
+        10,
+        'Tour name must be longer than or equal to 10 characters.',
+      ],
+      // this dude doesnt even allow spaces for the name so i'll leave it here just for demo of validator library
+      // validate: [isAlpha, 'Tour name must only contains alphabets.'] // method that validates if the input data are alphabets or not
     },
     slug: String,
     duration: {
-        type: Number,
-        required: [true, "A tour must have a duration."]
+      type: Number,
+      required: [true, 'A tour must have a duration.'],
     },
     maxGroupSize: {
-        type: Number,
-        required: [true, "A tour must have a group size that can fit."]
+      type: Number,
+      required: [true, 'A tour must have a group size that can fit.'],
     },
-    difficulty : {
-        type: String,
-        required: [true, "A tour should have a difficulty."],
-        // validator for difficulty
-        enum: {
-            values: ['easy', 'medium', 'difficult'],
-            message: 'Difficulty has to be either easy, medium or difficult.'
-        }
+    difficulty: {
+      type: String,
+      required: [true, 'A tour should have a difficulty.'],
+      // validator for difficulty
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty has to be either easy, medium or difficult.',
+      },
     },
     ratingsAverage: {
-        type: Number,
-        default: 4.5, // default value to show if we didn't specify the rating
-        min: [1, 'Rating must be above 1.0'],
-        max: [5, 'Rating must be below 5.0']
+      type: Number,
+      default: 4.5, // default value to show if we didn't specify the rating
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
-    ratingsQuantity : {
-        type: Number,
-        default: 0
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
     },
     price: {
-        type: Number,
-        required: [true, "A tour must have a price."]
+      type: Number,
+      required: [true, 'A tour must have a price.'],
     },
     priceDiscount: {
-        type: Number,
-        // custom validator
-        validate: {
-            validator: function(val) {
-                // return bool if the value we give as priceDiscount is less than actual price or not
-                // important to note that in this case, "this" keyword only refers to the created object, which means this validation is not gonna work for updating
-                return val < this.price; // of course it's gonna show error if the return value is false
-            },
-            message: 'Discount price ({VALUE}) must be less than regular price.' // ({VALUE}) is the placeholder for actual input discounr val
-        }
+      type: Number,
+      // custom validator
+      validate: {
+        validator: function (val) {
+          // return bool if the value we give as priceDiscount is less than actual price or not
+          // important to note that in this case, "this" keyword only refers to the created object, which means this validation is not gonna work for updating
+          return val < this.price; // of course it's gonna show error if the return value is false
+        },
+        message: 'Discount price ({VALUE}) must be less than regular price.', // ({VALUE}) is the placeholder for actual input discounr val
+      },
     },
     summary: {
-        type: String,
-        trim: true, 
-        required: [true, "A tour must have a summary."]
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a summary.'],
     },
     description: {
-        type: String,
-        trim: true
+      type: String,
+      trim: true,
     },
     imageCover: {
-        type: String, // this will be the name of image which we will read from the file system later
-        required: [true, "A tour must have a cover image."]
+      type: String, // this will be the name of image which we will read from the file system later
+      required: [true, 'A tour must have a cover image.'],
     },
     images: [String], // other images will be stored as an array of strings
     createdAt: {
-        type: Date, // set the datatype of date to check the timestamp of the tour to be created
-        // set the default date to now if the user didnt specify the create date
-        default: Date.now(), // this returns milliseconds in term of js, but mongo will change this into human readable
-        select: false // hide create date
+      type: Date, // set the datatype of date to check the timestamp of the tour to be created
+      // set the default date to now if the user didnt specify the create date
+      default: Date.now(), // this returns milliseconds in term of js, but mongo will change this into human readable
+      select: false, // hide create date
     },
     // different start dates for the same tour
     startDates: [Date],
     secretTour: {
-        type: Boolean,
-        default: false
-    }
-}, {
+      type: Boolean,
+      default: false,
+    },
+    startLocation: {
+      // In GeoJSON, the geospatial data structure is represented as an object.
+      // we need to use type as an object because it simply cant be plain String or Number,
+      // it's a geospatial data and it's type should be Point
+      // that's why we need to nest another type inside
+      type: {
+        type: String,
+        // we can specify multiple geometries like polygons, lines in MongoDB
+        // the default one is always Point
+        default: 'Point',
+        // but in this case, startLocation should be Point, so let's strict it with enum
+        enum: ['Point'],
+      },
+      // this is an array of numbers representing the coordinates of the point
+      // The order of the numbers depends on the coordinate reference system used (typically [longitude, latitude])
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    // embedded documents
+    locations: [
+      {
+        type: {
+          type: String,
+          default: 'Point',
+          enum: ['Point'],
+        },
+        coordinates: [Number],
+        address: String,
+        description: String,
+        day: Number,
+      },
+    ],
+  },
+  {
     // schema option to show virtual properties when the model is in the form of JSON or object
     toJSON: { virtuals: true },
-    toObject: { virtuals: true }
-});
+    toObject: { virtuals: true },
+  },
+);
 
 // virtual properties
 // we can use various calculations for the schema using virtual properties
 // NOTE: we can't use virtual properties in querying, because technically, they're not real fields in the document
 // They are only used for the user experience
 tourSchema.virtual('durationWeeks').get(function () {
-    return this.duration / 7; // calculate duration in weeks
+  return this.duration / 7; // calculate duration in weeks
 });
 
 // MongoDB Document middleware that runs before .save() and .create()
 // pre save hook
-tourSchema.pre('save', function(next) {
-    this.slug = slugify(this.name, { lower: true }); // slugify and shows the document's name
-    console.log(this.slug);
-    next();
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true }); // slugify and shows the document's name
+  console.log(this.slug);
+  next();
 });
 
 // // demo of document middleware pipeline
@@ -134,25 +176,25 @@ tourSchema.pre('save', function(next) {
 // use pre find hook to not show secret tours before saving to the DB
 // use regex to make sure the middleware is working for both find and findOne queries
 // ^find literally means all the queries start with find
-tourSchema.pre(/^find/, function(next) {
-    this.find({ secretTour: { $ne: true } }); // only shows non-secret tours
-    this.start = Date.now();
-    next();
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } }); // only shows non-secret tours
+  this.start = Date.now();
+  next();
 });
 
 // post find hook gets access to all the documents after the query has executed
-tourSchema.post(/^find/, function(docs, next) {
-    // calculate how long it takes to execute current query
-    console.log(`Query took ${Date.now() - this.start} milliseconds`); 
-    // console.log(docs);
-    next();
+tourSchema.post(/^find/, function (docs, next) {
+  // calculate how long it takes to execute current query
+  console.log(`Query took ${Date.now() - this.start} milliseconds`);
+  // console.log(docs);
+  next();
 });
 
 // MongoDB Aggregation Middleware
-tourSchema.pre('aggregate', function(next) {
-    // add another match to  filter out aggregated object pipeline method's secret tours
-    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } }); 
-    next();
+tourSchema.pre('aggregate', function (next) {
+  // add another match to  filter out aggregated object pipeline method's secret tours
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  next();
 });
 
 // create the model for the documents using specified schema rules
