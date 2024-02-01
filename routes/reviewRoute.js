7256465ec12322cfc1ea14a706729b4ae508cf42
JasontheOmnivorous <marsjason485@gmail.com>
2024-02-01 18:04:42 +0630

    route merging

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -4,7 +4,16 @@ const {
   createReview,
 } = require('../controllers/reviewController');
 const { protect, restrictTo } = require('./../controllers/authController');
-const router = express.Router();
+
+// this option enables route merging
+// so everytime user access /:tourId/reviews endpoint,
+// they're gonna be redirected to here
+// and every request will be handled here as long as req method matches
+// so, because of mergeParams is enabled,
+// the reviewRouter's inherits the redirected parent's params
+// so at the time the request reaches here,
+// the endpoint will be /:tourId/reviews
+const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -12,8 +12,23 @@ const {
   getSlug,
 } = require('./../controllers/tourController');
 const { protect, restrictTo } = require('./../controllers/authController');
-const { createReview } = require('../controllers/reviewController');
 const router = express.Router(); // create mount router for tours
+const reviewRouter = require('./reviewRoute');
+
+// POST => /tour/bfeuwg812e28/reviews
+// GET => /tour/bfeuwg812e28/reviews
+// GET => /tour/bfeuwg812e28/reviews/kjnueiwh39
+// router
+//   .route('/:tourId/reviews')
+//   .post(protect, restrictTo('user'), createReview);
+
+// route merging
+// this is basically saying, if we encounter an endpoint like this in th request,
+// use reviewRouter instead of this tourRouter
+// we wanna use reviewRouter here, because it's kinda confusing to use
+// tourRouter, even though reviews belong to tour
+// so, it's like redirecting
+router.use('/:tourId/reviews', reviewRouter);
 
 // // param middleware => a middleware that only runs for certain parameter in the URL
 // router.param('id', checkId);
@@ -40,12 +55,5 @@ router
   .get(getTour)
   .patch(updateTour)
   .delete(protect, restrictTo('admin', 'lead-guide'), deleteTour);
-
-// POST => /tour/bfeuwg812e28/reviews
-// GET => /tour/bfeuwg812e28/reviews
-// GET => /tour/bfeuwg812e28/reviews/kjnueiwh39
-router
-  .route('/:tourId/reviews')
-  .post(protect, restrictTo('user'), createReview);
 
 module.exports = router; // export our tour route
